220420df11c9ec251f65936b45556694369d2e56
Filip Todoroski <ftodoroski@ymail.com>
2019-11-17 21:38:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,7 +55,7 @@ def naming_system
         :sub_category => {
           :label => "Order",
           :sub_category => {
-            :label => "Family",
+            # :label => "Family",
             :sub_category => {
               :label => "Genus",
               :sub_category => {
