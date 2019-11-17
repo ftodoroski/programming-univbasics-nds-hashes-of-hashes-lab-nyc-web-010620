8c45b84cd40675a7a4079749e2b7e237c98d5a23
Filip Todoroski <ftodoroski@ymail.com>
2019-11-17 21:38:17 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -61,7 +61,7 @@ def naming_system
               :sub_category => {
                 :label => "Species",
                 :sub_category => {
-                  :label => nil
+                  
                 }
               }
             }
